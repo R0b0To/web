ce5ae3e8f56451102e6dfda377cef1a7e1913333
R0b0To <atonrhai@gmail.com>
2023-09-20 00:36:46 +0200

    up

--- server.js
+++ server.js
@@ -6,30 +6,33 @@ const app = express();
 
 // Create a Redis client
 const client = redis.createClient({
-  host: process.env.UPSTASH_HOST,
-  port: process.env.UPSTASH_PORT,
-  password: process.env.UPSTASH_PASSWORD,
+    host: process.env.UPSTASH_HOST,
+    port: process.env.UPSTASH_PORT,
+    password: process.env.UPSTASH_PASSWORD,
 });
 
 client.on('error', (err) => {
-  console.error('Redis Error:', err);
+    console.error('Redis Error:', err);
 });
 
-// Serve a simple webpage
-app.get('/', (req, res) => {
-  // Get the user's name from the database
-  client.get('user_name', (err, userName) => {
-    if (err) {
-      console.error('Error getting user name:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.send(`Hello, ${userName || 'Guest'}!`);
-    }
-  });
+// API endpoint to get the user name
+app.get('/getUserName', (req, res) => {
+    // Get the user's name from the database
+    client.get('user_name', (err, userName) => {
+        if (err) {
+            console.error('Error getting user name:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json({ userName });
+        }
+    });
 });
+
+// Serve your HTML file
+app.use(express.static('public'));
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
