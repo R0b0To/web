f83b0acd325c57450c0367b5568318018c26ec79
R0b0To <atonrhai@gmail.com>
2023-09-20 02:00:23 +0200

    update

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
-// server.js
-console.log('starting service')
-const express = require('express');
-const redis = require('redis');
-const app = express();
+import { Redis } from "@upstash/redis";
+import "isomorphic-fetch";
 
-// Create a Redis client
-const client = redis.createClient({
-    host: process.env.UPSTASH_HOST,
-    port: process.env.UPSTASH_PORT,
-    password: process.env.UPSTASH_PASSWORD,
-});
+const redis = Redis.fromEnv();
 
-client.on('error', (err) => {
-    console.error('Redis Error:', err);
-});
-
-// API endpoint to get the user name
-app.get('/getUserName', (req, res) => {
-    // Get the user's name from the database
-    client.get('user_name', (err, userName) => {
-        if (err) {
-            console.error('Error getting user name:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            res.json({ userName });
-        }
-    });
-});
-
-// Serve your HTML file
-app.use(express.static('public'));
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default async function handler(
+  _req,
+  res,
+) {
+  /**
+   * We're prefixing the key for our automated tests.
+   * This is to avoid collisions with other tests.
+   */
+  const key = ["vercel", process.env.VERCEL_GIT_COMMIT_SHA || "local", "nodejs"]
+    .join("_");
+  await redis.set(key, `${Math.floor(Math.random() * 100)}_hello 😋`);
+  const value = await redis.get(key);
+  res.json({ key, value });
+}
